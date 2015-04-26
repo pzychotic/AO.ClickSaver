a0f8780b60eb5a8c70150f00b7d6bb917258eb77
Thomas Roß <pzycho1980@gmail.com>
2015-04-26 17:33:37 +0200

    Initial commit.

---
+++ AO.ClickSaver/Source/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+
+namespace pzy.AO.ClickSaver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AO.ClickSaver/Source/MissionInfo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace pzy.AO.ClickSaver
+{
+    /// <summary>
+    /// Interaction logic for MissionInfo.xaml
+    /// </summary>
+    public partial class MissionInfo : UserControl
+    {
+        public MissionInfo()
+        {
+            InitializeComponent();
+        }
+
+        public void Clear()
+        {
+            _tbLocation.Text = "";
+            _tbType.Text = "";
+            _tbRewardItem.Text = "";
+            _tbFindItem.Text = "";
+            _tbExperience.Text = "";
+            _tbCash.Text = "";
+            _tbValue.Text = "";
+            _tbTotal.Text = "";
+        }
+    }
+}
