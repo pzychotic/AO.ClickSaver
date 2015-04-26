f25a39dbf55f45c145733507b2ca246c66a9fab8
Thomas Roß <pzycho1980@gmail.com>
2015-04-26 17:38:14 +0200

    Added WinApiWrapper with functions we'll need to find the AO window.

---
+++ AO.ClickSaver/Source/WinApiWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace pzy.AO.ClickSaver
+{
+    public class WinApiWrapper
+    {
+        public delegate bool EnumWindowsFunc( IntPtr hWnd, ref IntPtr hWndOut );
+
+        [DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+        [return: MarshalAs( UnmanagedType.Bool )]
+        public static extern bool EnumWindows( EnumWindowsFunc lpFunc, ref IntPtr hWnd );
+
+        [DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+        public static extern int GetWindowText( IntPtr hWnd, StringBuilder lpString, int nMaxCount );
+
+        [DllImport( "user32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+        public static extern int GetWindowTextLength( IntPtr hWnd );
+        
+        [DllImport( "user32.dll" )]
+        public static extern bool ClientToScreen( IntPtr hWnd, ref Point lpPoint );
+    }
+}
