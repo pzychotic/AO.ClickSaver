27c54c375373132f1aad7c6f3e56861950fb8ef6
Thomas Roß <pzycho1980@gmail.com>
2015-04-26 17:46:41 +0200

    Find the AO window and its coordinates on screen.

--- AO.ClickSaver/Source/MainWindow.xaml.cs
+++ AO.ClickSaver/Source/MainWindow.xaml.cs
@@ -15,5 +15,42 @@ namespace pzy.AO.ClickSaver
         {
             InitializeComponent();
         }
+
+        private void _btnStart_Click( object sender, RoutedEventArgs e )
+        {
+            // try to find the AO window, can't use FindWindow() here because
+            // the window title changes with the logged-in character name
+            IntPtr hWnd = new IntPtr();
+            WinApiWrapper.EnumWindows( new WinApiWrapper.EnumWindowsFunc( EnumWindowsCallback ), ref hWnd );
+
+            if( hWnd != IntPtr.Zero )
+            {
+                // get the top-left corner of the window in screen coordinates
+                System.Drawing.Point point = new System.Drawing.Point( 0, 0 );
+                WinApiWrapper.ClientToScreen( hWnd, ref point );
+            }
+        }
+
+        /// <summary>
+        /// Callback that gets called for every top-level window.
+        /// </summary>
+        /// <param name="hWnd">The window handle of the window.</param>
+        /// <param name="hWndOut">The window handle we will return.</param>
+        /// <returns>True to keep on enumerating, otherwise false.</returns>
+        private static bool EnumWindowsCallback( IntPtr hWnd, ref IntPtr hWndOut )
+        {
+            int titleLength = WinApiWrapper.GetWindowTextLength( hWnd );
+
+            StringBuilder sb = new StringBuilder( titleLength );
+            WinApiWrapper.GetWindowText( hWnd, sb, sb.Capacity );
+
+            if( sb.ToString().StartsWith( "Anarchy Online" ) )
+            {
+                hWndOut = hWnd;
+                return false;  // stop enumerating
+            }
+
+            return true;  // keep enumerating
+        }
     }
 }
